c4111b4cfe7325bade1ae1a0bc19eeba76950846
Diederick Niehorster <dcnieho@gmail.com>
2021-09-15 11:04:32 +0200

    split bunch of reusable functions from b_detectMarkers into utils.py

--- b_detectMarkers.py
+++ b_detectMarkers.py
@@ -1,99 +1,15 @@
-#!/usr/bin/python
-
-import sys
 from pathlib import Path
 
 import cv2
 import numpy as np
 import csv
 import math
-import pandas as pd
 from matplotlib import colors
 import time
-from shlex import shlex
+import utils
 
 gFPSFac         = 1
 
-
-class Marker:
-    def __init__(self, key, center, corners=None, color=None):
-        self.key = key
-        self.center = center
-        self.corners = corners
-        self.color = color
-
-    def __str__(self):
-        ret = '[%s]: center @ (%.2f, %.2f)' % (self.key, self.center[0], self.center[1])
-        return ret
-
-def getValidationSetup(configDir):
-    # read key=value pairs into dict
-    with open(str(configDir / "validationSetup.txt")) as f:
-        lexer = shlex(f)
-        lexer.whitespace += '='
-        lexer.wordchars += '.'  # don't split extensions of filenames in the input file
-        validationSetup = dict(zip(lexer, lexer))
-
-    # parse numerics into int or float
-    for key,val in validationSetup.items():
-        if np.all([c.isdigit() for c in val]):
-            validationSetup[key] = int(val)
-        else:
-            try:
-                validationSetup[key] = float(val)
-            except:
-                pass # just keep value as a string
-    return validationSetup
-
-def getKnownMarkers(configDir, validationSetup):
-    """ (0,0) is at center target, (-,-) bottom left """
-    cellSizeCm = 2.*math.tan(math.radians(.5))*validationSetup['distance']
-    markerHalfSizeCm = cellSizeCm*validationSetup['markerSide']/2.
-            
-    # read in target positions
-    markers = {}
-    targets = pd.read_csv(str(configDir / validationSetup['targetPosFile']),index_col=0,names=['x','y','clr'])
-    center  = targets.loc[validationSetup['centerTarget'],['x','y']]
-    targets.x = cellSizeCm * (targets.x.astype('float32') - center.x)
-    targets.y = cellSizeCm * (targets.y.astype('float32') - center.y)
-    for idx, row in targets.iterrows():
-        key = 't%d' % idx
-        markers[key] = Marker(key, row[['x','y']].values, color=row.clr)
-    
-    # read in aruco marker positions
-    markerPos = pd.read_csv(str(configDir / validationSetup['markerPosFile']),index_col=0,names=['x','y'])
-    markerPos.x = cellSizeCm * (markerPos.x.astype('float32') - center.x)
-    markerPos.y = cellSizeCm * (markerPos.y.astype('float32') - center.y)
-    for idx, row in markerPos.iterrows():
-        key = '%d' % idx
-        c   = row[['x','y']].values
-        # top left first, and clockwise: same order as detected aruco marker corners
-        tl = c + np.array( [ -markerHalfSizeCm ,  markerHalfSizeCm ] )
-        tr = c + np.array( [  markerHalfSizeCm ,  markerHalfSizeCm ] )
-        br = c + np.array( [  markerHalfSizeCm , -markerHalfSizeCm ] )
-        bl = c + np.array( [ -markerHalfSizeCm , -markerHalfSizeCm ] )
-        markers[key] = Marker(key, c, corners=[ tl, tr, br, bl ])
-    
-    # determine bounding box of markers ([left, top, right, bottom])
-    bbox = []
-    bbox.append(markerPos.x.min()-markerHalfSizeCm)
-    bbox.append(markerPos.y.max()+markerHalfSizeCm) # top is at positive
-    bbox.append(markerPos.x.max()+markerHalfSizeCm)
-    bbox.append(markerPos.y.min()-markerHalfSizeCm) # bottom is at negative
-
-    return markers, bbox
-
-def toNormPos(x,y,bbox):
-    extents = [bbox[2]-bbox[0], math.fabs(bbox[3]-bbox[1])]             # math.fabs to deal with bboxes where (-,-) is bottom left
-    pos     = [(x-bbox[0])/extents[0], math.fabs(y-bbox[1])/extents[1]] # math.fabs to deal with bboxes where (-,-) is bottom left
-    return pos
-
-def toImagePos(x,y,bbox,imSize,drawShift):
-    # fractional position between bounding boxe edges, (0,0) in bottom left
-    pos = toNormPos(x,y, bbox)
-    # turn into int, add margin
-    pos = tuple([int(round(a*b)*drawShift) for a,b in zip(pos,imSize)])
-    return pos
 
 def storeReferenceBoard(referenceBoard,inputDir,validationSetup,knownMarkers,markerBBox):
     # get image with markers
@@ -113,7 +29,7 @@ def storeReferenceBoard(referenceBoard,inputDir,validationSetup,knownMarkers,mar
     for key in knownMarkers:
         if key.startswith('t'):
             # 1. determine position on image
-            circlePos = toImagePos(*knownMarkers[key].center, markerBBox,[refBoardWidth,refBoardHeight],drawShift)
+            circlePos = utils.toImagePos(*knownMarkers[key].center, markerBBox,[refBoardWidth,refBoardHeight],drawShift)
 
             # 2. draw
             clr = tuple([int(i*255) for i in colors.to_rgb(knownMarkers[key].color)[::-1]])  # need BGR color ordering
@@ -143,47 +59,12 @@ def estimateTransform(known, detectedCorners, detectedIDs):
     return h, True
 
 
-def transform(h, x, y):
-    src = np.float32([[ [x,y] ]])
-    dst = cv2.perspectiveTransform(src,h)
-    return dst[0][0]
-
-
-def distortPoint(p, cameraMatrix, distCoeff):
-    fx = cameraMatrix[0][0]
-    fy = cameraMatrix[1][1]
-    cx = cameraMatrix[0][2]
-    cy = cameraMatrix[1][2]
-
-    k1 = distCoeff[0]
-    k2 = distCoeff[1]
-    k3 = distCoeff[4]
-    p1 = distCoeff[2]
-    p2 = distCoeff[3]
-
-    x = (p[0] - cx) / fx
-    y = (p[1] - cy) / fy
-
-    r2 = x*x + y*y
-
-    dx = x * (1 + k1 * r2 + k2 * r2 * r2 + k3 * r2 * r2 * r2)
-    dy = y * (1 + k1 * r2 + k2 * r2 * r2 + k3 * r2 * r2 * r2)
-
-    dx = dx + (2 * p1 * x * y + p2 * (r2 + 2 * x * x))
-    dy = dy + (p1 * (r2 + 2 * y * y) + 2 * p2 * x * y)
-
-    p[0] = dx * fx + cx;
-    p[1] = dy * fy + cy;
-
-    return p
-
-
 def process(inputDir,basePath):
     global gFPSFac
 
     configDir = basePath / "config"
     # open file with information about Aruco marker and Gaze target locations
-    validationSetup = getValidationSetup(configDir)
+    validationSetup = utils.getValidationSetup(configDir)
     
     # open video file, query it for size
     inVideo = str(inputDir / 'worldCamera.mp4')
@@ -197,7 +78,7 @@ def process(inputDir,basePath):
     # get info about markers on our board
     # Aruco markers have numeric keys, gaze targets have keys starting with 't'
     aruco_dict   = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
-    knownMarkers, markerBBox = getKnownMarkers(configDir, validationSetup)
+    knownMarkers, markerBBox = utils.getKnownMarkers(configDir, validationSetup)
     centerTarget = knownMarkers['t%d'%validationSetup['centerTarget']].center
     
     # turn into aruco board object to be used for pose estimation
@@ -260,8 +141,8 @@ def process(inputDir,basePath):
                     
                     # find where target is expected to be in the image
                     iH = np.linalg.inv(H)
-                    target = transform(iH, centerTarget[0], centerTarget[1])
-                    target = distortPoint( target, cameraMatrix, distCoeff)
+                    target = utils.transform(iH, centerTarget[0], centerTarget[1])
+                    target = utils.distortPoint( target, cameraMatrix, distCoeff)
                     
                     # draw target location on image
                     drawShift = 8   # for sub-pixel positioning
---
+++ utils.py
@@ -0,0 +1,131 @@
+import math
+import cv2
+import numpy as np
+import pandas as pd
+from shlex import shlex
+
+
+class Marker:
+    def __init__(self, key, center, corners=None, color=None):
+        self.key = key
+        self.center = center
+        self.corners = corners
+        self.color = color
+
+    def __str__(self):
+        ret = '[%s]: center @ (%.2f, %.2f)' % (self.key, self.center[0], self.center[1])
+        return ret
+
+def getValidationSetup(configDir):
+    # read key=value pairs into dict
+    with open(str(configDir / "validationSetup.txt")) as f:
+        lexer = shlex(f)
+        lexer.whitespace += '='
+        lexer.wordchars += '.'  # don't split extensions of filenames in the input file
+        validationSetup = dict(zip(lexer, lexer))
+
+    # parse numerics into int or float
+    for key,val in validationSetup.items():
+        if np.all([c.isdigit() for c in val]):
+            validationSetup[key] = int(val)
+        else:
+            try:
+                validationSetup[key] = float(val)
+            except:
+                pass # just keep value as a string
+    return validationSetup
+
+def getKnownMarkers(configDir, validationSetup):
+    """ (0,0) is at center target, (-,-) bottom left """
+    cellSizeCm = 2.*math.tan(math.radians(.5))*validationSetup['distance']
+    markerHalfSizeCm = cellSizeCm*validationSetup['markerSide']/2.
+            
+    # read in target positions
+    markers = {}
+    targets = pd.read_csv(str(configDir / validationSetup['targetPosFile']),index_col=0,names=['x','y','clr'])
+    center  = targets.loc[validationSetup['centerTarget'],['x','y']]
+    targets.x = cellSizeCm * (targets.x.astype('float32') - center.x)
+    targets.y = cellSizeCm * (targets.y.astype('float32') - center.y)
+    for idx, row in targets.iterrows():
+        key = 't%d' % idx
+        markers[key] = Marker(key, row[['x','y']].values, color=row.clr)
+    
+    # read in aruco marker positions
+    markerPos = pd.read_csv(str(configDir / validationSetup['markerPosFile']),index_col=0,names=['x','y'])
+    markerPos.x = cellSizeCm * (markerPos.x.astype('float32') - center.x)
+    markerPos.y = cellSizeCm * (markerPos.y.astype('float32') - center.y)
+    for idx, row in markerPos.iterrows():
+        key = '%d' % idx
+        c   = row[['x','y']].values
+        # top left first, and clockwise: same order as detected aruco marker corners
+        tl = c + np.array( [ -markerHalfSizeCm ,  markerHalfSizeCm ] )
+        tr = c + np.array( [  markerHalfSizeCm ,  markerHalfSizeCm ] )
+        br = c + np.array( [  markerHalfSizeCm , -markerHalfSizeCm ] )
+        bl = c + np.array( [ -markerHalfSizeCm , -markerHalfSizeCm ] )
+        markers[key] = Marker(key, c, corners=[ tl, tr, br, bl ])
+    
+    # determine bounding box of markers ([left, top, right, bottom])
+    bbox = []
+    bbox.append(markerPos.x.min()-markerHalfSizeCm)
+    bbox.append(markerPos.y.max()+markerHalfSizeCm) # top is at positive
+    bbox.append(markerPos.x.max()+markerHalfSizeCm)
+    bbox.append(markerPos.y.min()-markerHalfSizeCm) # bottom is at negative
+
+    return markers, bbox
+
+def toNormPos(x,y,bbox):
+    # transforms input (x,y) which is in image units (e.g. cm on an aruco board)
+    # to a normalized position in the image, given the image's bounding box in
+    # image units
+    # (0,0) in bottom left
+
+    extents = [bbox[2]-bbox[0], math.fabs(bbox[3]-bbox[1])]             # math.fabs to deal with bboxes where (-,-) is bottom left
+    pos     = [(x-bbox[0])/extents[0], math.fabs(y-bbox[1])/extents[1]] # math.fabs to deal with bboxes where (-,-) is bottom left
+    return pos
+
+def toImagePos(x,y,bbox,imSize,drawShift=1):
+    # transforms input (x,y) which is in image units (e.g. cm on an aruco board)
+    # to a pixel position in the image, given the image's bounding box in
+    # image units
+    # takes into account OpenCV's drawshift for subpixel positioning (see docs of
+    # e.g. cv2.circle)
+
+    # fractional position between bounding box edges, (0,0) in bottom left
+    pos = toNormPos(x,y, bbox)
+    # turn into int, add margin
+    pos = tuple([int(round(a*b)*drawShift) for a,b in zip(pos,imSize)])
+    return pos
+
+def transform(h, x, y):
+    src = np.float32([[ [x,y] ]])
+    dst = cv2.perspectiveTransform(src,h)
+    return dst[0][0]
+
+
+def distortPoint(p, cameraMatrix, distCoeff):
+    fx = cameraMatrix[0][0]
+    fy = cameraMatrix[1][1]
+    cx = cameraMatrix[0][2]
+    cy = cameraMatrix[1][2]
+
+    k1 = distCoeff[0]
+    k2 = distCoeff[1]
+    k3 = distCoeff[4]
+    p1 = distCoeff[2]
+    p2 = distCoeff[3]
+
+    x = (p[0] - cx) / fx
+    y = (p[1] - cy) / fy
+
+    r2 = x*x + y*y
+
+    dx = x * (1 + k1 * r2 + k2 * r2 * r2 + k3 * r2 * r2 * r2)
+    dy = y * (1 + k1 * r2 + k2 * r2 * r2 + k3 * r2 * r2 * r2)
+
+    dx = dx + (2 * p1 * x * y + p2 * (r2 + 2 * x * x))
+    dy = dy + (p1 * (r2 + 2 * y * y) + 2 * p2 * x * y)
+
+    p[0] = dx * fx + cx;
+    p[1] = dy * fy + cy;
+
+    return p
