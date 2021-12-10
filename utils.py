a88d331d3952f86aee7254619328b81ef0754422
Diederick Niehorster <dcnieho@gmail.com>
2021-12-10 10:43:58 +0100

    simplify utils.angle_between (if there is a np syntax version, why write it out by hand?)

--- utils.py
+++ utils.py
@@ -173,15 +173,8 @@ def undistortPoint(x, y, cameraMatrix, distCoeff):
     return dst[0][0]
 
 
-def angle_between(v1, v2):
-    #(180.0 / math.pi) * math.atan2(np.linalg.norm(np.cross(v1,v2)), np.dot(v1,v2))
-
-    cx = v1[1] * v2[2] - v1[2] * v2[1]
-    cy = v1[2] * v2[0] - v1[0] * v1[2]
-    cz = v1[0] * v2[1] - v1[1] * v2[0]
-    cross = math.sqrt( cx * cx + cy * cy + cz * cz )
-    dot = v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2] 
-    return (180.0 / math.pi) * math.atan2( cross, dot )
+def angle_between(v1, v2): 
+    return (180.0 / math.pi) * math.atan2(np.linalg.norm(np.cross(v1,v2)), np.dot(v1,v2))
 
 def intersect_plane_ray(planeNormal, planePoint, rayDirection, rayPoint, epsilon=1e-6):
     # from https://rosettacode.org/wiki/Find_the_intersection_of_a_line_with_a_plane#Python
