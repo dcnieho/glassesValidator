9e862582301e7c5cc1ea0efe420abfa5696dbbdd
Diederick Niehorster <dcnieho@gmail.com>
2021-12-12 00:03:14 +0100

    e_calculateDataQuality: can now do data quality determinations for multiple analysis intervals

--- e_calculateDataQuality.py
+++ e_calculateDataQuality.py
@@ -22,14 +22,14 @@ def process(inputDir,basePath):
     validationSetup = utils.getValidationSetup(configDir)
 
     # get interval coded to be analyzed
-    analyzeStartFrame,analyzeEndFrame = utils.getAnalysisInterval(inputDir / "analysisInterval.tsv")
-    assert(analyzeStartFrame is not None and analyzeEndFrame is not None)
+    analyzeFrames = utils.getAnalysisIntervals(inputDir / "analysisInterval.tsv")
+    assert(analyzeFrames is not None)
 
     # Read pose of marker board
-    rVec,tVec = utils.getMarkerBoardPose(inputDir / 'boardPose.tsv',analyzeStartFrame,analyzeEndFrame,True)
+    rVec,tVec = utils.getMarkerBoardPose(inputDir / 'boardPose.tsv',analyzeFrames[0],analyzeFrames[-1],True)
 
     # Read gaze on board data
-    gazeWorld = utils.getGazeWorldData(inputDir / 'gazeWorldPos.tsv',analyzeStartFrame,analyzeEndFrame,True)
+    gazeWorld = utils.getGazeWorldData(inputDir / 'gazeWorldPos.tsv',analyzeFrames[0],analyzeFrames[-1],True)
 
     # get info about markers on our board
     # Aruco markers have numeric keys, gaze targets have keys starting with 't'
@@ -53,161 +53,164 @@ def process(inputDir,basePath):
     opt['maxMergeTime']     = 81        # ms
     opt['minFixDur']        = 50        # ms
     # 2. collect data
-    data = {}
-    data['time'] = np.array([s.ts for v in gazeWorld.values() for s in v])-gazeWorld[analyzeStartFrame][0].ts
-    data['L_X']  = np.array([s.lGaze2D[0] for v in gazeWorld.values() for s in v])
-    data['L_Y']  = np.array([s.lGaze2D[1] for v in gazeWorld.values() for s in v])
-    data['R_X']  = np.array([s.rGaze2D[0] for v in gazeWorld.values() for s in v])
-    data['R_Y']  = np.array([s.rGaze2D[1] for v in gazeWorld.values() for s in v])
-    # 3. run event classification to find fixations
-    fix,dat,par = I2MC.I2MC(data,opt)
+    for ival in range(0,len(analyzeFrames)//2):
+        gazeWorldToAnal = {k:v for (k,v) in gazeWorld.items() if k>=analyzeFrames[ival*2] and k<=analyzeFrames[ival*2+1]}
+        data = {}
+        data['time'] = np.array([s.ts for v in gazeWorldToAnal.values() for s in v])-gazeWorldToAnal[analyzeFrames[ival*2]][0].ts
+        data['L_X']  = np.array([s.lGaze2D[0] for v in gazeWorldToAnal.values() for s in v])
+        data['L_Y']  = np.array([s.lGaze2D[1] for v in gazeWorldToAnal.values() for s in v])
+        data['R_X']  = np.array([s.rGaze2D[0] for v in gazeWorldToAnal.values() for s in v])
+        data['R_Y']  = np.array([s.rGaze2D[1] for v in gazeWorldToAnal.values() for s in v])
+        # 3. run event classification to find fixations
+        fix,dat,par = I2MC.I2MC(data,opt)
 
-    # for each target, find closest fixation
-    minDur      = 150       # ms
-    used        = np.zeros((fix['start'].size),dtype='bool')
-    selected    = np.empty((len(targets),),dtype='int')
-    selected[:] = -1
+        # for each target, find closest fixation
+        minDur      = 150       # ms
+        used        = np.zeros((fix['start'].size),dtype='bool')
+        selected    = np.empty((len(targets),),dtype='int')
+        selected[:] = -1
 
-    for i,t in zip(range(len(targets)),targets):
-        nUsed = np.invert(used)
+        for i,t in zip(range(len(targets)),targets):
+            nUsed = np.invert(used)
 
-        # select fixation
-        dist = np.hypot(fix['xpos']-targets[t][0], fix['ypos']-targets[t][1])
-        dist[fix['dur'][nUsed]<minDur] = math.inf   # make sure that fixations that are too short are not selected
-        selected[i] = np.argmin(dist)
+            # select fixation
+            dist = np.hypot(fix['xpos']-targets[t][0], fix['ypos']-targets[t][1])
+            dist[fix['dur'][nUsed]<minDur] = math.inf   # make sure that fixations that are too short are not selected
+            selected[i] = np.argmin(dist)
     
-    # 2. calculate offsets from targets
-    # a. determine which samples to process (those during selected fixation)
-    whichTarget     = np.empty((len(data['time']),),dtype='int')
-    whichTarget[:]  = -1
-    for i,t in zip(range(len(selected)),targets):
-        whichTarget[fix['start'][selected[i]]:fix['end'][selected[i]]+1] = t
-    # b. get offset per sample
-    offset = np.empty((2,2,len(whichTarget),))
-    offset[:] = np.nan
-    frameIdxs = [k for k,v in gazeWorld.items() for s in v]
-    oriL  = np.vstack([s.lGazeOrigin for v in gazeWorld.values() for s in v])
-    oriR  = np.vstack([s.rGazeOrigin for v in gazeWorld.values() for s in v])
-    gaze3L= np.vstack([s.lGaze3D     for v in gazeWorld.values() for s in v])
-    gaze3R= np.vstack([s.rGaze3D     for v in gazeWorld.values() for s in v])
-    gaze2L= np.vstack([s.lGaze2D     for v in gazeWorld.values() for s in v])
-    gaze2R= np.vstack([s.rGaze2D     for v in gazeWorld.values() for s in v])
-    for s in range(len(whichTarget)):
-        t = whichTarget[s]
-        if t==-1:
-            continue
-
-        for e in range(2):
-            if e==0:
-                ori         = oriL[s,:]
-                gaze        = gaze3L[s,:]
-                gazeBoard   = gaze2L[s,:]
-            else:
-                ori         = oriR[s,:]
-                gaze        = gaze3R[s,:]
-                gazeBoard   = gaze2R[s,:]
-
-            if frameIdxs[s] not in rVec:
+        # 2. calculate offsets from targets
+        # a. determine which samples to process (those during selected fixation)
+        whichTarget     = np.empty((len(data['time']),),dtype='int')
+        whichTarget[:]  = -1
+        for i,t in zip(range(len(selected)),targets):
+            whichTarget[fix['start'][selected[i]]:fix['end'][selected[i]]+1] = t
+        # b. get offset per sample
+        offset = np.empty((2,2,len(whichTarget),))
+        offset[:] = np.nan
+        frameIdxs = [k for k,v in gazeWorldToAnal.items() for s in v]
+        oriL  = np.vstack([s.lGazeOrigin for v in gazeWorldToAnal.values() for s in v])
+        oriR  = np.vstack([s.rGazeOrigin for v in gazeWorldToAnal.values() for s in v])
+        gaze3L= np.vstack([s.lGaze3D     for v in gazeWorldToAnal.values() for s in v])
+        gaze3R= np.vstack([s.rGaze3D     for v in gazeWorldToAnal.values() for s in v])
+        gaze2L= np.vstack([s.lGaze2D     for v in gazeWorldToAnal.values() for s in v])
+        gaze2R= np.vstack([s.rGaze2D     for v in gazeWorldToAnal.values() for s in v])
+        for s in range(len(whichTarget)):
+            t = whichTarget[s]
+            if t==-1:
                 continue
 
-            RBoard  = cv2.Rodrigues(rVec[frameIdxs[s]])[0]
-            RtBoard = np.hstack((RBoard, tVec[frameIdxs[s]].reshape(3,1)))
-            target  = np.matmul(RtBoard,np.array([targets[t][0], targets[t][1], 0., 1.]))
+            for e in range(2):
+                if e==0:
+                    ori         = oriL[s,:]
+                    gaze        = gaze3L[s,:]
+                    gazeBoard   = gaze2L[s,:]
+                else:
+                    ori         = oriR[s,:]
+                    gaze        = gaze3R[s,:]
+                    gazeBoard   = gaze2R[s,:]
+
+                if frameIdxs[s] not in rVec:
+                    continue
+
+                RBoard  = cv2.Rodrigues(rVec[frameIdxs[s]])[0]
+                RtBoard = np.hstack((RBoard, tVec[frameIdxs[s]].reshape(3,1)))
+                target  = np.matmul(RtBoard,np.array([targets[t][0], targets[t][1], 0., 1.]))
             
-            # get vectors from origin to target and to gaze point
-            vGaze   = gaze  -ori;
-            vTarget = target-ori;
+                # get vectors from origin to target and to gaze point
+                vGaze   = gaze  -ori;
+                vTarget = target-ori;
         
-            # get offset
-            ang2D           = utils.angle_between(vTarget,vGaze)
-            # decompose in horizontal/vertical (in board space)
-            onBoardAngle    = math.atan2(gazeBoard[1]-targets[t][1],gazeBoard[0]-targets[t][0])
-            offset[:,e,s]   = ang2D*np.array([math.cos(onBoardAngle), math.sin(onBoardAngle)])
+                # get offset
+                ang2D           = utils.angle_between(vTarget,vGaze)
+                # decompose in horizontal/vertical (in board space)
+                onBoardAngle    = math.atan2(gazeBoard[1]-targets[t][1],gazeBoard[0]-targets[t][0])
+                offset[:,e,s]   = ang2D*np.array([math.cos(onBoardAngle), math.sin(onBoardAngle)])
 
-    # Add average of the two eyes
-    offset = np.concatenate((offset,np.mean(offset,axis=1,keepdims=True)),axis=1)
+        # Add average of the two eyes
+        offset = np.concatenate((offset,np.mean(offset,axis=1,keepdims=True)),axis=1)
 
-    # determine order in which targets were looked at
-    u,i=np.unique(whichTarget, return_index=True)
-    i=np.delete(i,u==-1)
-    u=np.delete(u,u==-1)
-    i=np.argsort(i)
-    lookOrder = u[i]
+        # determine order in which targets were looked at
+        u,i=np.unique(whichTarget, return_index=True)
+        i=np.delete(i,u==-1)
+        u=np.delete(u,u==-1)
+        i=np.argsort(i)
+        lookOrder = u[i]
 
-    # 3. calculate metrics per target
-    accuracy2D = np.empty((len(targets),2,3))       # nTarget x [X Y] x nEye (L,R,Avg)
-    accuracy2D[:] = np.nan
-    accuracy1D = np.empty((len(targets),3))         # nTarget x nEye (L,R,Avg)
-    accuracy1D[:] = np.nan
+        # 3. calculate metrics per target
+        accuracy2D = np.empty((len(targets),2,3))       # nTarget x [X Y] x nEye (L,R,Avg)
+        accuracy2D[:] = np.nan
+        accuracy1D = np.empty((len(targets),3))         # nTarget x nEye (L,R,Avg)
+        accuracy1D[:] = np.nan
     
-    RMS2D = np.empty((len(targets),2,3))            # nTarget x [X Y] x nEye (L,R,Avg)
-    RMS2D[:] = np.nan
-    RMS1D = np.empty((len(targets),3))              # nTarget x nEye (L,R,Avg)
-    RMS1D[:] = np.nan
+        RMS2D = np.empty((len(targets),2,3))            # nTarget x [X Y] x nEye (L,R,Avg)
+        RMS2D[:] = np.nan
+        RMS1D = np.empty((len(targets),3))              # nTarget x nEye (L,R,Avg)
+        RMS1D[:] = np.nan
     
-    STD2D = np.empty((len(targets),2,3))            # nTarget x [X Y] x nEye (L,R,Avg)
-    STD2D[:] = np.nan
-    STD1D = np.empty((len(targets),3))              # nTarget x nEye (L,R,Avg)
-    STD1D[:] = np.nan
+        STD2D = np.empty((len(targets),2,3))            # nTarget x [X Y] x nEye (L,R,Avg)
+        STD2D[:] = np.nan
+        STD1D = np.empty((len(targets),3))              # nTarget x nEye (L,R,Avg)
+        STD1D[:] = np.nan
     
-    dataLoss = np.empty((len(targets),3))           # nTarget x nEye (L,R,Avg)
-    dataLoss[:] = np.nan
-    for i,t in zip(range(len(selected)),targets):
-        qData = whichTarget==t
+        dataLoss = np.empty((len(targets),3))           # nTarget x nEye (L,R,Avg)
+        dataLoss[:] = np.nan
+        for i,t in zip(range(len(selected)),targets):
+            qData = whichTarget==t
         
-        accuracy2D[i,:,:] = np.nanmean(offset[:,:,qData],axis=2)
-        accuracy1D[i,:]   = np.nanmean(np.hypot(offset[0,:,qData],offset[1,:,qData]),axis=0)
+            accuracy2D[i,:,:] = np.nanmean(offset[:,:,qData],axis=2)
+            accuracy1D[i,:]   = np.nanmean(np.hypot(offset[0,:,qData],offset[1,:,qData]),axis=0)
     
-        RMS2D[i,:,:]      = np.sqrt(np.nanmean(np.diff(offset[:,:,qData],axis=2)**2,axis=2))
-        RMS1D[i,:]        = np.hypot(RMS2D[i,0,:],RMS2D[i,1,:])
+            RMS2D[i,:,:]      = np.sqrt(np.nanmean(np.diff(offset[:,:,qData],axis=2)**2,axis=2))
+            RMS1D[i,:]        = np.hypot(RMS2D[i,0,:],RMS2D[i,1,:])
     
-        STD2D[i,:,:]      = np.nanstd(offset[:,:,qData],axis=2,ddof=1)
-        STD1D[i,:]        = np.hypot(STD2D[i,0,:],STD2D[i,1,:])
+            STD2D[i,:,:]      = np.nanstd(offset[:,:,qData],axis=2,ddof=1)
+            STD1D[i,:]        = np.hypot(STD2D[i,0,:],STD2D[i,1,:])
     
-        dataLoss[i,:]     = np.sum(np.isnan(offset[0,:,qData]),axis=0)/np.sum(qData)
+            dataLoss[i,:]     = np.sum(np.isnan(offset[0,:,qData]),axis=0)/np.sum(qData)
         
-    # organize for output and write to file
-    df = pd.DataFrame()
-    df.index.name = 'target'
-    for i,t in zip(range(len(selected)),targets):
-        df.loc[t,'pos_x'] = targets[t][0]
-        df.loc[t,'pos_y'] = targets[t][1]
-        df.loc[t,'order'] = lookOrder[i]
-        df.loc[t,'acc_left_x'] = accuracy2D[i,0,0]
-        df.loc[t,'acc_left_y'] = accuracy2D[i,1,0]
-        df.loc[t,'acc_left'] = accuracy1D[i,0]
-        df.loc[t,'acc_right_x'] = accuracy2D[i,0,1]
-        df.loc[t,'acc_right_y'] = accuracy2D[i,1,1]
-        df.loc[t,'acc_right'] = accuracy1D[i,1]
-        df.loc[t,'acc_avg_x'] = accuracy2D[i,0,2]
-        df.loc[t,'acc_avg_y'] = accuracy2D[i,1,2]
-        df.loc[t,'acc_avg'] = accuracy1D[i,2]
+        # organize for output and write to file
+        df = pd.DataFrame()
+        df.index.name = 'target'
+        for i,t in zip(range(len(selected)),targets):
+            df.loc[t,'interval'] = ival+1
+            df.loc[t,'pos_x'] = targets[t][0]
+            df.loc[t,'pos_y'] = targets[t][1]
+            df.loc[t,'order'] = lookOrder[i]
+            df.loc[t,'acc_left_x'] = accuracy2D[i,0,0]
+            df.loc[t,'acc_left_y'] = accuracy2D[i,1,0]
+            df.loc[t,'acc_left'] = accuracy1D[i,0]
+            df.loc[t,'acc_right_x'] = accuracy2D[i,0,1]
+            df.loc[t,'acc_right_y'] = accuracy2D[i,1,1]
+            df.loc[t,'acc_right'] = accuracy1D[i,1]
+            df.loc[t,'acc_avg_x'] = accuracy2D[i,0,2]
+            df.loc[t,'acc_avg_y'] = accuracy2D[i,1,2]
+            df.loc[t,'acc_avg'] = accuracy1D[i,2]
             
-        df.loc[t,'rms_left_x'] = RMS2D[i,0,0]
-        df.loc[t,'rms_left_y'] = RMS2D[i,1,0]
-        df.loc[t,'rms_left'] = RMS1D[i,0]
-        df.loc[t,'rms_right_x'] = RMS2D[i,0,1]
-        df.loc[t,'rms_right_y'] = RMS2D[i,1,1]
-        df.loc[t,'rms_right'] = RMS1D[i,1]
-        df.loc[t,'rms_avg_x'] = RMS2D[i,0,2]
-        df.loc[t,'rms_avg_y'] = RMS2D[i,1,2]
-        df.loc[t,'rms_avg'] = RMS1D[i,2]
+            df.loc[t,'rms_left_x'] = RMS2D[i,0,0]
+            df.loc[t,'rms_left_y'] = RMS2D[i,1,0]
+            df.loc[t,'rms_left'] = RMS1D[i,0]
+            df.loc[t,'rms_right_x'] = RMS2D[i,0,1]
+            df.loc[t,'rms_right_y'] = RMS2D[i,1,1]
+            df.loc[t,'rms_right'] = RMS1D[i,1]
+            df.loc[t,'rms_avg_x'] = RMS2D[i,0,2]
+            df.loc[t,'rms_avg_y'] = RMS2D[i,1,2]
+            df.loc[t,'rms_avg'] = RMS1D[i,2]
             
-        df.loc[t,'std_left_x'] = STD2D[i,0,0]
-        df.loc[t,'std_left_y'] = STD2D[i,1,0]
-        df.loc[t,'std_left'] = STD1D[i,0]
-        df.loc[t,'std_right_x'] = STD2D[i,0,1]
-        df.loc[t,'std_right_y'] = STD2D[i,1,1]
-        df.loc[t,'std_right'] = STD1D[i,1]
-        df.loc[t,'std_avg_x'] = STD2D[i,0,2]
-        df.loc[t,'std_avg_y'] = STD2D[i,1,2]
-        df.loc[t,'std_avg'] = STD1D[i,2]
+            df.loc[t,'std_left_x'] = STD2D[i,0,0]
+            df.loc[t,'std_left_y'] = STD2D[i,1,0]
+            df.loc[t,'std_left'] = STD1D[i,0]
+            df.loc[t,'std_right_x'] = STD2D[i,0,1]
+            df.loc[t,'std_right_y'] = STD2D[i,1,1]
+            df.loc[t,'std_right'] = STD1D[i,1]
+            df.loc[t,'std_avg_x'] = STD2D[i,0,2]
+            df.loc[t,'std_avg_y'] = STD2D[i,1,2]
+            df.loc[t,'std_avg'] = STD1D[i,2]
             
-        df.loc[t,'dataLoss_left'] = dataLoss[i,0]
-        df.loc[t,'dataLoss_right'] = dataLoss[i,1]
-        df.loc[t,'dataLoss_avg'] = dataLoss[i,2]
+            df.loc[t,'dataLoss_left'] = dataLoss[i,0]
+            df.loc[t,'dataLoss_right'] = dataLoss[i,1]
+            df.loc[t,'dataLoss_avg'] = dataLoss[i,2]
 
-    df.to_csv(str(inputDir / 'dataQuality.tsv'), sep='\t', na_rep='nan', float_format="%.3f")
+        df.to_csv(str(inputDir / 'dataQuality.tsv'), mode='w' if ival==0 else 'a', header=ival==0, sep='\t', na_rep='nan', float_format="%.3f")
 
 if __name__ == '__main__':
     basePath = Path(__file__).resolve().parent
