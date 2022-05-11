a8947a245568028874113d6ae22dc33892074062
Diederick Niehorster <dcnieho@gmail.com>
2022-05-11 15:07:47 +0200

    e1_computeOffsetsToTargets: update and add case of only gaze position on board and assumed viewing distance

--- e1_computeOffsetsToTargets.py
+++ e1_computeOffsetsToTargets.py
@@ -36,7 +36,7 @@ def process(inputDir,basePath):
         return
 
     # Read pose of marker board
-    rVec,tVec = utils.BoardPose.readDataFromFile(inputDir / 'boardPose.tsv',analyzeFrames[0],analyzeFrames[-1],True)[0:2]
+    poses = utils.BoardPose.readDataFromFile(inputDir / 'boardPose.tsv',analyzeFrames[0],analyzeFrames[-1],True)
 
     # Read gaze on board data
     gazeWorld = utils.GazeWorld.readDataFromFile(inputDir / 'gazeWorldPos.tsv',analyzeFrames[0],analyzeFrames[-1],True)
@@ -50,7 +50,7 @@ def process(inputDir,basePath):
     # (angle) of gaze (each eye) to each of the targets
     for ival in range(0,len(analyzeFrames)//2):
         gazeWorldToAnal = {k:v for (k,v) in gazeWorld.items() if k>=analyzeFrames[ival*2] and k<=analyzeFrames[ival*2+1]}
-        frameIdxs = [k for k,v in gazeWorldToAnal.items() for s in v]
+        frameIdxs       =           [k                for k,v in gazeWorldToAnal.items()  for s in v]
         ts              = np.vstack([s.ts               for v in gazeWorldToAnal.values() for s in v])
         oriLeft         = np.vstack([s.lGazeOrigin      for v in gazeWorldToAnal.values() for s in v])
         oriRight        = np.vstack([s.rGazeOrigin      for v in gazeWorldToAnal.values() for s in v])
@@ -63,16 +63,17 @@ def process(inputDir,basePath):
         gaze3DHomography= np.vstack([s.gaze3DHomography for v in gazeWorldToAnal.values() for s in v])
         gaze2DHomography= np.vstack([s.gaze2DHomography for v in gazeWorldToAnal.values() for s in v])
 
-        offset = np.empty((oriLeft.shape[0],4,len(targets),2))
+        offset = np.empty((oriLeft.shape[0],5,len(targets),2))
         offset[:] = np.nan
 
         for s in range(oriLeft.shape[0]):
-            if frameIdxs[s] not in rVec:
+            if frameIdxs[s] not in poses:
                 continue
-            RBoard  = cv2.Rodrigues(rVec[frameIdxs[s]])[0]
-            RtBoard = np.hstack((RBoard, tVec[frameIdxs[s]].reshape(3,1)))
+            RBoard  = cv2.Rodrigues(poses[frameIdxs[s]].rVec)[0]
+            RtBoard = np.hstack((RBoard, poses[frameIdxs[s]].tVec.reshape(3,1)))
 
-            for e in range(4):
+            # all based on pose info
+            for e in range(5):
                 if e==0:
                     ori         = oriLeft[s,:]
                     gaze        = gaze3DLeft[s,:]
@@ -86,37 +87,45 @@ def process(inputDir,basePath):
                     ori         = np.zeros(3)
                     gaze        = gaze3DRay[s,:]
                     gazeBoard   = gaze2DRay[s,:]
-                elif e==3:
+                elif e in [3, 4]:
                     # from camera perspective, using homography
                     ori         = np.zeros(3)
                     gaze        = gaze3DHomography[s,:]
                     gazeBoard   = gaze2DHomography[s,:]
 
                 for ti,t in enumerate(targets):
-                    target  = np.matmul(RtBoard,np.array([targets[t][0], targets[t][1], 0., 1.]))
+                    if e==4:
+                        # get vectors based on assumed viewing distance (from config), without using pose info
+                        distMm  = validationSetup['distance']*10.
+                        vGaze   = np.array([gazeBoard[0] , gazeBoard[1] , distMm])
+                        vTarget = np.array([targets[t][0], targets[t][1], distMm])
+                    else:
+                        # use known 3D vectors
+                        target  = np.matmul(RtBoard,np.array([targets[t][0], targets[t][1], 0., 1.]))
             
-                    # get vectors from origin to target and to gaze point
-                    vGaze   = gaze  -ori;
-                    vTarget = target-ori;
+                        # get vectors from origin to target and to gaze point
+                        vGaze   = gaze  -ori
+                        vTarget = target-ori
         
                     # get offset
                     ang2D           = utils.angle_between(vTarget,vGaze)
                     # decompose in horizontal/vertical (in board space)
                     onBoardAngle    = math.atan2(gazeBoard[1]-targets[t][1],gazeBoard[0]-targets[t][0])
                     offset[s,e,ti,:]= ang2D*np.array([math.cos(onBoardAngle), math.sin(onBoardAngle)])
-        
+
         # organize for output and write to file
         # 1. create cartesian product of sample index, eye and target indices
         # order of inputs needed to get expected output is a mystery to me, but screw it, works
-        dat = cartesian_product(np.arange(4),np.arange(offset.shape[0]),[t for t in targets])
+        dat = cartesian_product(np.arange(5),np.arange(offset.shape[0]),[t for t in targets])
         # 2. put into data frame
         df = pd.DataFrame()
         df['timestamp'] = ts[dat[:,1],0]
         df['marker_interval'] = ival+1
-        map = ['left','right','ray','homography']
+        map = ['left','right','fromRay','homography_and_pose','homography_and_viewDist']
         df['eye'] = [map[e] for e in dat[:,0]]
         df['target'] = dat[:,2]
         df = pd.concat([df, pd.DataFrame(np.reshape(offset,(-1,2)),columns=['offset_x','offset_y'])],axis=1)
+        df = df.dropna(axis=0, subset=['offset_x','offset_y'])  # drop any missing data
         # 3. write to file
         df.to_csv(str(inputDir / 'gazeTargetOffset.tsv'), mode='w' if ival==0 else 'a', header=ival==0, index=False, sep='\t', na_rep='nan', float_format="%.3f")
 
