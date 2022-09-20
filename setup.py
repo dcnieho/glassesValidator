5ac4edc74fe45d5b2d86dac8ac51301280cc8f0d
Diederick Niehorster <dcnieho@gmail.com>
2022-09-21 00:08:44 +0200

    requirements.txt: figured out format for git subdirectory that also works with setup.py (adjusted setup.py code to make it work)

--- setup.py
+++ setup.py
@@ -17,18 +17,27 @@ dependency_links = []
 
 # Do not add to required lines pointing to Git repositories
 EGG_MARK = '#egg='
+EGG_MARK2= '&egg='  # in case of subdirectories
 for line in requirements:
     if line.startswith('-e git:') or line.startswith('-e git+') or \
             line.startswith('git:') or line.startswith('git+'):
         line = line.lstrip('-e ')  # in case that is using "-e"
-        if EGG_MARK in line:
-            package_name = line[line.find(EGG_MARK) + len(EGG_MARK):]
-            repository = line[:line.find(EGG_MARK)]
+        if EGG_MARK in line or EGG_MARK2 in line:
+            if EGG_MARK in line:
+                idx = line.find(EGG_MARK)
+            else:
+                idx = line.find(EGG_MARK2)
+            package_name = line[idx + len(EGG_MARK):]
+            repository = line[:idx]
             required.append('%s @ %s' % (package_name, repository))
-            dependency_links.append(line)
+            if EGG_MARK2 in line:
+                dependency_links.append(line.replace(EGG_MARK2,EGG_MARK))
+            else:
+                dependency_links.append(line)
         else:
             print('Dependency to a git repository should have the format:')
-            print('git+ssh://git@github.com/xxxxx/xxxxxx#egg=package_name')
+            print('git+ssh://git@github.com/xxxxx/xxxxxx#egg=package_name, or')
+            print('git+ssh://git@github.com/xxxxx/xxxxxx#subdirectory=sub_dir&egg=package_name')
     else:
         required.append(line)
 
