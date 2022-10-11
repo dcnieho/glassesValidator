c48baf9e55baba829d0b436a531a1d2d470fd4a4
Diederick Niehorster <dcnieho@gmail.com>
2022-10-11 23:30:25 +0200

    untested first try at a github release workflow that'll build the executables and release the package to PyPI

---
+++ .github/workflow_data/release.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import os
+
+
+if __name__ == "__main__":
+    with open(os.environ["GITHUB_EVENT_PATH"]) as f:
+        event = json.load(f)
+    print(f"event = {json.dumps(event, indent=4)}")
+    release = requests.get(
+        event["release"]["url"],
+        headers={
+            "Accept": "application/vnd.github.v3+json",
+            "Authorization": f"token {os.environ['GITHUB_TOKEN']}"
+        }
+    ).json()
+    print(f"release = {json.dumps(release, indent=4)}")
+    body = "## ⬇️ Download\n"
+    for asset_type, asset_icon in [("Windows", "🪟"), ("Linux", "🐧"), ("MacOS", "🍎"), ("Source", "🐍")]:
+        print(f"Adding {asset_type}")
+        for asset in release["assets"]:
+            if asset_type.lower() in asset["name"].lower():
+                asset_url = asset["browser_download_url"]
+        body += f">### [{asset_type} {asset_icon}]({asset_url})\n\n"
+    body += (
+        "## 🚀 Changelog\n" +
+        release["body"]
+    )
+    print(f"Full body:\n\n{body}")
+    req = requests.patch(
+        release["url"],
+        headers={
+            "Accept": "application/vnd.github.v3+json",
+            "Authorization": f"token {os.environ['GITHUB_TOKEN']}"
+        },
+        json={
+            "body": body
+        }
+    )
+    if not req.ok:
+        print(f"{req.status_code = }\n{req.content = }")
