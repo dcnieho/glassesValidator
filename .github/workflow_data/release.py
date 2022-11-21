f2e6489052e4d5dc76c612f8bb8b1c2e5a00e345
Diederick Niehorster <dcnieho@gmail.com>
2022-11-21 10:31:19 +0100

    build.yml: separate wheel creation from upload to PyPI task, so that running the release text creator script only depends on wheel creation, not PyPI upload.

--- .github/workflow_data/release.py
+++ .github/workflow_data/release.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     ).json()
     print(f"release = {json.dumps(release, indent=4)}")
     body = "## ⬇️ Download\n"
-    for asset_type, asset_icon in [("Windows", "🪟"), ("Linux", "🐧"), ("MacOS", "🍎"), ("Wheel", "🐍"), ("Source", "🐍")]:
+    for asset_type, asset_icon in [("Windows", "🪟"), ("Linux", "🐧"), ("MacOS", "🍎"), ("Wheel", "📦"), ("Source", "🐍")]:
         print(f"Adding {asset_type}")
         for asset in release["assets"]:
             if asset_type.lower() in asset["name"].lower() or (asset_type=="Wheel" and asset["name"].endswith('.whl')):
