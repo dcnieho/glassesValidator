5b81a2980f1508d8fc53a6920a180beef3ad0c90
Diederick Niehorster <dcnieho@gmail.com>
2022-12-04 22:36:11 +0100

    references should be raw strings, else backslash in bibtex wouldn't work

--- src/glassesValidator/GUI/_impl/globals.py
+++ src/glassesValidator/GUI/_impl/globals.py
@@ -15,8 +15,8 @@ version           = __version__
 pypi_page         = "https://pypi.org/project/glassesValidator/"
 github_page       = "https://github.com/dcnieho/glassesValidator"
 developer_page    = "https://scholar.google.se/citations?user=uRUYoVgAAAAJ&hl=en"
-reference         = "Niehorster, D.C., Hessels, R.S., Benjamins, J.S., Nyström, M. & Hooge, I.T.C. (submitted). GlassesValidator: Data quality tool for eye tracking glasses."
-reference_bibtex  = """@article{niehorster2023glassesValidator,
+reference         = r"Niehorster, D.C., Hessels, R.S., Benjamins, J.S., Nyström, M. & Hooge, I.T.C. (submitted). GlassesValidator: Data quality tool for eye tracking glasses."
+reference_bibtex  = r"""@article{niehorster2023glassesValidator,
     Author = {Niehorster, Diederick C. and Hessels, Roy S. and Benjamins, Jeroen S. and Nystr{\"o}m, Marcus and Hooge, Ignace T. C.},
     Journal = {},
     Number = {},
