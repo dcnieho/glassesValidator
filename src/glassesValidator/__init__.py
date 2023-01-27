f2b11bbaf667b6fcb7653da7558f8177e7342d4e
Diederick Niehorster <dcnieho@gmail.com>
2023-01-27 15:32:59 +0100

    with the (ongoing) switch to the imgui-based simple image viewer, we can nw use the headless version of opencv

--- src/glassesValidator/__init__.py
+++ src/glassesValidator/__init__.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import sys
-if sys.platform.startswith("linux"):
-    # something imported later in glassesValidator causes opencv windows not to show
-    # unless one was already briefly created (not just cv2 imported first), on Linux.
-    # so below is a shitty patch making things work.
-    import cv2
-    cv2.namedWindow("dummy",cv2.WINDOW_NORMAL)
-    cv2.destroyAllWindows()
-
 from . import config
 from . import GUI
 from . import preprocess
