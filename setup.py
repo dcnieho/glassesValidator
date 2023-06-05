15504d6b80cf0eb715d2aac75e59c411cd3170ae
Diederick Niehorster <dcnieho@gmail.com>
2023-06-05 14:17:33 +0200

    more specific python version support

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setuptools.setup(
         "Source Code": main_ns['__url__'],
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
