ed90319469a3bf2e1f8ce3b1fecc83286d9d210f
Diederick Niehorster <dcnieho@gmail.com>
2024-05-05 21:28:30 +0200

    attempt to make precompiled exe work with packaged ffmpeg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+import os
 import multiprocessing
 
 if getattr(sys, "frozen", False):
@@ -10,6 +11,11 @@ if getattr(sys, "frozen", False):
     # doesn't encounter a None in site.USER_BASE
     import site
     site.getuserbase()
+
+    # need to put packaged ffmpeg executable on path
+    p = os.path.join(os.path.dirname(sys.executable),'lib')
+    os.environ["PATH"] = p + os.pathsep + os.environ["PATH"]
+    os.add_dll_directory(p)
 
 if __name__=="__main__":
     multiprocessing.freeze_support()
