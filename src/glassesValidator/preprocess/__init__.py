292ffef3674b3635c91918487543d98bedc25d7f
Diederick Niehorster <dcnieho@gmail.com>
2024-05-27 16:04:44 +0200

    preprocess: split off what is generic of do_import() to glassesTools

--- src/glassesValidator/preprocess/__init__.py
+++ src/glassesValidator/preprocess/__init__.py
@@ -6,48 +6,30 @@ from glassesTools.recording  import Recording
 from glassesTools.eyetracker import EyeTracker
 
 from glassesTools.importing import adhawk_mindlink, pupil_core, pupil_invisible, pupil_neon, SeeTrue_STONE, SMI_ETG, tobii_G2, tobii_G3
-from glassesTools.importing import check_device, check_source_dir, check_output_dir, get_recording_info
+from glassesTools.importing import check_device, do_import as _do_impt, get_recording_info
 
 from .. import utils
 
 # single front end to the various device import functions. Step 1 of our 3-step process
 def do_import(output_dir: str | pathlib.Path = None, source_dir: str | pathlib.Path = None, device: str | EyeTracker = None, rec_info: Recording = None) -> Recording:
+    # output_dir is the folder in which the working directory will be created
     if rec_info is not None:
         if isinstance(rec_info,list):
             raise ValueError('You should provide a single Recording to this function''s "rec_info" input argument, not a list of Recordings.')
 
     device, rec_info = check_device(device, rec_info)
-    source_dir, rec_info = check_source_dir(source_dir, rec_info)
-
     # ensure there is an output directory
     if rec_info is not None:
         if not rec_info.working_directory:
             rec_info.working_directory = output_dir / make_fs_dirname(rec_info, output_dir)
-    output_dir, rec_info = check_output_dir(None, rec_info)
 
-    # do the actual import/pre-process
-    match device:
-        case EyeTracker.AdHawk_MindLink:
-            rec_info = adhawk_mindlink(output_dir, source_dir, rec_info)
-        case EyeTracker.Pupil_Core:
-            rec_info = pupil_core(output_dir, source_dir, rec_info)
-        case EyeTracker.Pupil_Invisible:
-            rec_info = pupil_invisible(output_dir, source_dir, rec_info)
-        case EyeTracker.Pupil_Neon:
-            rec_info = pupil_neon(output_dir, source_dir, rec_info)
-        case EyeTracker.SeeTrue_STONE:
-            rec_info = SeeTrue_STONE(output_dir, source_dir, rec_info)
-        case EyeTracker.SMI_ETG:
-            rec_info = SMI_ETG(output_dir, source_dir, rec_info)
-        case EyeTracker.Tobii_Glasses_2:
-            rec_info = tobii_G2(output_dir, source_dir, rec_info)
-        case EyeTracker.Tobii_Glasses_3:
-            rec_info = tobii_G3(output_dir, source_dir, rec_info)
+    # do the import
+    rec_info = _do_impt(None, source_dir, device, rec_info)
 
     # make sure there is a processing status file
-    utils.get_recording_status(output_dir, create_if_missing=True)
+    utils.get_recording_status(rec_info.working_directory, create_if_missing=True)
     # write status file to indicate import finished
-    utils.update_recording_status(output_dir, utils.Task.Imported, utils.Status.Finished)
+    utils.update_recording_status(rec_info.working_directory, utils.Task.Imported, utils.Status.Finished)
 
     return rec_info
 
