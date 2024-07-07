eaf50a2ea5e930047ca0d4dfa5e26d0501cd37e9
Diederick Niehorster <dcnieho@gmail.com>
2024-07-07 21:44:39 +0200

    d_gazeToPoster: now using worldgaze_gui.show_visualization() for showing gaze mapped to the validator poster

--- src/glassesValidator/process/d_gazeToPoster.py
+++ src/glassesValidator/process/d_gazeToPoster.py
@@ -3,7 +3,7 @@ import pathlib
 import cv2
 import threading
 
-from glassesTools import drawing, gaze_headref, gaze_worldref, intervals, ocv, plane, recording, timestamps
+from glassesTools import drawing, gaze_headref, gaze_worldref, intervals, ocv, plane, recording, timestamps, worldgaze_gui
 from glassesTools.video_gui import GUI, generic_tooltip_drawer, qns_tooltip
 
 from .. import config
@@ -11,7 +11,7 @@ from .. import utils
 
 
 stopAllProcessing = False
-def process(working_dir, config_dir=None, show_visualization=False, show_poster=True, show_only_intervals=True):
+def process(working_dir, config_dir=None, show_visualization=True, show_poster=True, show_only_intervals=True):
     # if show_visualization, each frame is shown in a viewer, overlaid with info about detected planes and projected gaze
     # if show_poster, gaze in poster space is also drawn in a separate window
     # if show_only_intervals, only the coded validation episodes (if available) are shown in the viewer while the rest of the scene video is skipped past
@@ -27,11 +27,8 @@ def process(working_dir, config_dir=None, show_visualization=False, show_poster=
         gui.set_interesting_keys('qns')
         gui.register_draw_callback('status',lambda: generic_tooltip_drawer(qns_tooltip()))
         frame_win_id = gui.add_window(working_dir.name)
-        poster_win_id= None
-        if show_poster:
-            poster_win_id = gui.add_window("poster")
 
-        proc_thread = threading.Thread(target=do_the_work, args=(working_dir, config_dir, gui, frame_win_id, show_poster, poster_win_id, show_only_intervals))
+        proc_thread = threading.Thread(target=do_the_work, args=(working_dir, config_dir, gui, frame_win_id, show_poster, show_only_intervals))
         proc_thread.start()
         gui.start()
         proc_thread.join()
@@ -40,7 +37,7 @@ def process(working_dir, config_dir=None, show_visualization=False, show_poster=
         return do_the_work(working_dir, config_dir, None, None, False, None, False)
 
 
-def do_the_work(working_dir, config_dir, gui, frame_win_id, show_poster, poster_win_id, show_only_intervals):
+def do_the_work(working_dir, config_dir, gui, frame_win_id, show_poster, show_only_intervals):
     global stopAllProcessing
 
     utils.update_recording_status(working_dir, utils.Task.Gaze_Tranformed_To_Poster, utils.Status.Running)
@@ -52,13 +49,13 @@ def do_the_work(working_dir, config_dir, gui, frame_win_id, show_poster, poster_
     analyzeFrames   = utils.readMarkerIntervalsFile(working_dir / "markerInterval.tsv")
 
     # Read gaze data
-    gazes,maxFrameIdx = gaze_headref.read_dict_from_file(working_dir / 'gazeData.tsv', episodes=analyzeFrames if not gui or show_only_intervals else None)
+    head_gazes  = gaze_headref.read_dict_from_file(working_dir / 'gazeData.tsv', episodes=analyzeFrames if not gui or show_only_intervals else None)[0]
 
     # Read camera pose w.r.t. poster
     poses       = plane.read_dict_from_file(working_dir / 'posterPose.tsv', episodes=analyzeFrames if not gui or show_only_intervals else None)
 
     # transform
-    plane_gazes = gaze_worldref.gazes_head_to_world(poses, gazes, cameraParams)
+    plane_gazes = gaze_worldref.gazes_head_to_world(poses, head_gazes, cameraParams)
 
     # store to file
     gaze_worldref.write_dict_to_file(plane_gazes, working_dir / 'gazePosterPos.tsv', skip_missing=True)
@@ -70,78 +67,12 @@ def do_the_work(working_dir, config_dir, gui, frame_win_id, show_poster, poster_
     if gui is None:
         return False
 
-    # prep visualizations
-    # get info about recording
-    recInfo         = recording.Recording.load_from_json(working_dir)
-    # open file with information about ArUco marker and Gaze target locations
+    in_video = recording.Recording.load_from_json(working_dir).get_scene_video_path()
     validationSetup = config.get_validation_setup(config_dir)
     poster          = config.poster.Poster(config_dir, validationSetup)
-
-    cap             = ocv.CV2VideoReader(recInfo.get_scene_video_path(), timestamps.VideoTimestamps(working_dir / 'frameTimestamps.tsv').timestamps)
-    width           = cap.get_prop(cv2.CAP_PROP_FRAME_WIDTH)
-    height          = cap.get_prop(cv2.CAP_PROP_FRAME_HEIGHT)
-
-    subPixelFac = 8   # for sub-pixel positioning
-    stopAllProcessing = False
-    for frame_idx in range(maxFrameIdx+1):
-        done, frame, frame_idx, frame_ts = cap.read_frame(report_gap=True)
-        if done or (show_only_intervals and intervals.beyond_last_interval(frame_idx, analyzeFrames)):
-            break
-
-        keys = gui.get_key_presses()
-        if 'q' in keys:
-            # quit fully
-            stopAllProcessing = True
-            break
-        if 'n' in keys:
-            # goto next
-            break
-
-        # check we're in a current interval, else skip processing
-        # NB: have to spool through like this, setting specific frame to read
-        # with cap.get(cv2.CAP_PROP_POS_FRAMES) doesn't seem to work reliably
-        # for VFR video files
-        if show_only_intervals and not intervals.is_in_interval(frame_idx, analyzeFrames):
-            # no need to show this frame
-            continue
-
-        if show_poster:
-            refImg = poster.get_ref_image(400)
-
-
-        if frame_idx in gazes:
-            for gaze_head, gaze_world in zip(gazes[frame_idx],plane_gazes[frame_idx]):
-                # draw gaze point on scene video
-                gaze_head.draw(frame, cameraParams, subPixelFac)
-
-                # draw plane gazes on video and poster
-                if frame_idx in poses:
-                    gaze_world.drawOnWorldVideo(frame, cameraParams, subPixelFac)
-                    if show_poster:
-                        gaze_world.drawOnPlane(refImg, poster, subPixelFac)
-
-        if show_poster:
-            gui.update_image(refImg, frame_ts/1000., frame_idx, window_id = poster_win_id)
-
-        # if we have poster pose, draw poster origin on video
-        if frame_idx in poses:
-            a = poses[frame_idx].getOriginOnImage(cameraParams)
-            drawing.openCVCircle(frame, a, 3, (0,255,0), -1, subPixelFac)
-            drawing.openCVLine(frame, (a[0],0), (a[0],height), (0,255,0), 1, subPixelFac)
-            drawing.openCVLine(frame, (0,a[1]), (width,a[1]) , (0,255,0), 1, subPixelFac)
-
-        # keys is populated above
-        if 's' in keys:
-            # screenshot
-            cv2.imwrite(working_dir / f'calc_frame_{frame_idx}.png', frame)
-
-        gui.update_image(frame, frame_ts/1000., frame_idx, window_id = frame_win_id)
-        closed, = gui.get_state()
-        if closed:
-            stopAllProcessing = True
-            break
-
-
-    gui.stop()
-
-    return stopAllProcessing
+    return worldgaze_gui.show_visualization(
+        working_dir,
+        in_video, working_dir / 'frameTimestamps.tsv', working_dir / "calibration.xml",
+        {'poster': poster}, {'poster': poses}, head_gazes, {'poster': plane_gazes}, {'poster': analyzeFrames},
+        gui, frame_win_id, show_poster, show_only_intervals, 8
+    )
