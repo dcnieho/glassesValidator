5c4149e19b2964661efa2d0b81a335c8d27ceb49
Diederick Niehorster <dcnieho@gmail.com>
2024-09-20 09:24:39 +0200

    missed another case of non fontawesome icons

--- src/glassesValidator/GUI/_impl/gui.py
+++ src/glassesValidator/GUI/_impl/gui.py
@@ -612,7 +612,7 @@ class MainGUI():
         self.update_recordings()
         self.init_imgui_glfw(is_reload=is_reload)
         if globals.project_path is not None:
-            task_column = recording_table.ColumnSpec(2,ifa6.ICON_FA_SQUARE_POLL_VERTICAL+" Status",imgui.TableColumnFlags_.no_resize,draw_recording_status_widget,lambda iid: task_names.index(self.recordings[iid].task.value),"󱀩")
+            task_column = recording_table.ColumnSpec(2,ifa6.ICON_FA_SQUARE_POLL_VERTICAL+" Status",imgui.TableColumnFlags_.no_resize,draw_recording_status_widget,lambda iid: task_names.index(self.recordings[iid].task.value),ifa6.ICON_FA_SQUARE_POLL_VERTICAL)
             self.recording_list = recording_table.RecordingTable(globals.recordings, globals.selected_recordings, [task_column], draw_recordings_context_menu, empty_space_context_menu, remove_recording)
 
     def update_recordings(self, subset=None):
